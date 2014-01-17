4572b89fe87b23d6ee7be2dcf3e0ebe71e4d80e9
SeungyoupBaek <bsy8220@naver.com>
2014-01-17 14:29:27 +0900

    GPS 실습 완료했습니다.

--- GPSManagerApp/ViewController.m
+++ GPSManagerApp/ViewController.m
@@ -7,16 +7,53 @@
 //
 
 #import "ViewController.h"
+#import "TMapView.h"
 
-@interface ViewController ()
+#define APP_KEY @"7b7b1456-6496-3c3e-ae92-3cf87fd15065"
+#define TOOLBAR_HEIGHT 70
+
+@interface ViewController ()<TMapGpsManagerDelegate, TMapViewDelegate>
+
+@property (strong, nonatomic) TMapGpsManager *gpsManager;
+@property (strong, nonatomic) TMapView *mapView;
 
 @end
 
 @implementation ViewController
 
+#pragma mark GPS Manager's Delegate
+-(void)locationChanged:(TMapPoint *)newTmp{
+    NSLog(@"Location Changed : %@", newTmp);
+    [self.mapView setCenterPoint:newTmp];
+}
+
+-(void)headingChanged:(double)heading{
+    
+}
+
+- (IBAction)switchGPS:(id)sender {
+    UISwitch *_switch = (UISwitch *)sender;
+    
+    if (YES == _switch.on) {
+        [self.gpsManager openGps];
+    }else{
+        [self.gpsManager closeGps];
+    }
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    // 툴바의 크기를 고려한 mapView
+    CGRect rect = CGRectMake(0, TOOLBAR_HEIGHT, self.view.frame.size.width, self.view.frame.size.height - TOOLBAR_HEIGHT);
+    self.mapView = [[TMapView alloc] initWithFrame:rect];
+    self.mapView.zoomLevel = 12.0;
+    [self.view addSubview:self.mapView];
+    
+    self.gpsManager = [[TMapGpsManager alloc] init];
+    [self.gpsManager setDelegate:self];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
